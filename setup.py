8bd0d890f49b916cbcccf8889f015778799a6501
ralex0 <ralex0@users.noreply.github.com>
2018-04-05 22:18:49 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='example',
+      version='1.0',
+      description='An example package for Harvard PHYS201 Sp2018',
+      url='http://github.com/phys201/example',
+      author='ralex0',
+      author_email='ralex0@users.noreply.github.com',
+      license='GPLv3',
+      packages=['pizza'],
+      install_requires=['numpy'])
