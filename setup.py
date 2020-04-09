4fe8b44a97c6bc5b9a3c4cb2a77c609c43c88660
barkls <barkley@g.harvard.edu>
2020-04-09 11:23:32 -0400

    update to 2020

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='example',
       version='1.0',
-      description='An example package for Harvard PHYS201 Sp2018',
+      description='An example package for Harvard PHYS201',
       url='http://github.com/phys201/example',
       author='ralex0',
       author_email='ralex0@users.noreply.github.com',
