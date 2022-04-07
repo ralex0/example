d30122226808f9b3bd5cffedc1e38519f5eca7cc
jennmcguire <55767313+jennmcguire@users.noreply.github.com>
2022-04-07 18:45:06 -0400

    Update data_io.py
    updating the get_example_data_file_path function to use pathlib instead of os

--- example/data_io.py
+++ example/data_io.py
@@ -1,20 +1,15 @@
-import os
 import pandas as pd
+from pathlib import Path
 
 
 def get_example_data_file_path(filename, data_dir='example_data'):
-    # __file__ is the location of the source file currently in use (so
+    # Path.cwd() returns the location of the source file currently in use (so
     # in this case io.py). We can use it as base path to construct
     # other paths from that should end up correct on other machines or
     # when the package is installed
-    start = os.path.abspath(__file__)
-    start_dir = os.path.dirname(start)
-    # If you need to go up another directory (for example if you have
-    # this function in your tests directory and your data is in the
-    # package directory one level up) you can use
-    # up_dir = os.path.split(start_dir)[0]
-    data_dir = os.path.join(start_dir, data_dir)
-    return os.path.join(start_dir, data_dir, filename)
+    current_directory = Path.cwd()
+    data_path = Path(current_directory, data_dir, filename)
+    return data_path
 
 
 def load_data(data_file):
